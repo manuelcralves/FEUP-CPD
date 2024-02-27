69c5cd4371617452b436ba018f71b54b00a23390
Manuel Alves <up201906910@up.pt>
2024-02-27 17:34:44 +0000

    OnMultLine

--- assign1/src/App.java
+++ assign1/src/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-    public static void onMult(int m_ar, int m_br) {
+    public static void OnMult(int m_ar, int m_br) {
 
         long Time1, Time2;
 
@@ -54,6 +54,57 @@ public class App {
         System.out.println("\n");
     }
 
+    public static void OnMultLine(int m_ar, int m_br) {
+        long Time1, Time2;
+
+        int i, j, k;
+
+        double temp;
+
+        double[][] pha = new double[m_ar][m_ar];
+        double[][] phb = new double[m_ar][m_ar];
+        double[][] phc = new double[m_ar][m_ar];
+
+        for (i = 0; i < m_ar;i++){
+            for (j = 0; j < m_ar; j++){
+                pha[i][j] = 1.0;
+            }
+        }
+
+        for (i = 0; i < m_br;i++) {
+            for (j = 0; j < m_br;j++){
+                phb[i][j] = i+1;
+            }
+        }
+
+        Time1 = System.nanoTime();
+
+        for (i = 0; i < m_ar;i++){
+            for (k = 0; k < m_br;k++){
+                for (j = 0; j < m_ar;j++){
+                    phc[i][j] += pha[i][k] * phb[k][j];
+                }
+            }
+        }
+
+        Time2 = System.nanoTime();
+
+        long Time = (Time2 - Time1)/1000000;
+
+        double TimeDouble = (double) Time / 1000;
+
+        System.out.printf("Time: %3.3f seconds\n",TimeDouble);
+
+        System.out.println("Result Matrix:");
+        for (i = 0; i < 1;i++){
+            for (j = 0; j < Math.min(10,m_br);j++){
+                System.out.print(phc[i][j] + " ");
+            }
+        }
+        System.out.println("\n");
+
+    }
+
     public static void main(String[] args) throws Exception {
         int lin, col, blockSize;
         int op = 1;
@@ -74,9 +125,10 @@ public class App {
 
             switch (op) {
                 case 1:
-                    onMult(lin,col);
+                    OnMult(lin,col);
                     break;
                 case 2:
+                    OnMultLine(lin,col);
                     break;
                 case 3:
                     break;
