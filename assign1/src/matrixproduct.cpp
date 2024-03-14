697eb30cc006f31a70c95f91926033308ffae414
up202107987 <up202107987@fe.up.pt>
2024-03-14 10:31:22 +0000

    finish part2

--- assign1/src/matrixproduct.cpp
+++ assign1/src/matrixproduct.cpp
@@ -14,7 +14,8 @@ using namespace std;
  
 void OnMult(int m_ar, int m_br) 
 {
-	
+	SYSTEMTIME Time1, Time2;
+
 	char st[100];
 	double temp;
 	int i, j, k;
@@ -39,13 +40,11 @@ void OnMult(int m_ar, int m_br)
 
 
 
-    double Time1 = omp_get_wtime();
+    Time1 = clock();
 
-	#pragma omp parallel private(i, j, k)
 	for(i=0; i<m_ar; i++)
 	{	for( j=0; j<m_br; j++)
 		{	temp = 0;
-			#pragma omp for
 			for( k=0; k<m_ar; k++)
 				{	
 					temp += pha[i*m_ar+k] * phb[k*m_br+j];
@@ -55,7 +54,7 @@ void OnMult(int m_ar, int m_br)
 	}
 
 
-    double Time2 = omp_get_wtime();
+    Time2 = clock();
 	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
 	cout << st;
 
@@ -77,7 +76,6 @@ void OnMult(int m_ar, int m_br)
 // add code here for line x line matriz multiplication
 void OnMultLine(int m_ar, int m_br)
 {
- 	SYSTEMTIME Time1, Time2;
 	
 	char st[100];
 	int i, j, k;
@@ -89,6 +87,7 @@ void OnMultLine(int m_ar, int m_br)
 	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
 
 	memset(phc, 0, (m_ar * m_ar) * sizeof(double));
+
 
 	for(i=0; i<m_ar; i++)
 		for(j=0; j<m_ar; j++)
@@ -102,8 +101,9 @@ void OnMultLine(int m_ar, int m_br)
 
 
 
-    Time1 = clock();
+    double Time1 = omp_get_wtime();
 
+	#pragma omp parallel private(i, j, k)
 	for(i=0; i<m_ar; i++)
 	{	for(j=0; j<m_br; j++)
 		{
@@ -114,7 +114,65 @@ void OnMultLine(int m_ar, int m_br)
 		}
 	}
 
-    Time2 = clock();
+    double Time2 = omp_get_wtime();
+	sprintf(st, "Time: %3.3f ", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+    // display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);   
+}
+
+void OnMultLine2(int m_ar, int m_br)
+{
+	
+	char st[100];
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	memset(phc, 0, (m_ar * m_ar) * sizeof(double));
+
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+
+
+    double Time1 = omp_get_wtime();
+
+	#pragma omp parallel private(i, j, k)
+	for(i=0; i<m_ar; i++)
+	{	for(j=0; j<m_br; j++)
+		{
+			#pragma omp for
+			for(k=0; k<m_ar; k++)
+			{	
+				phc[i*m_ar+k] += pha[i*m_ar+j] * phb[j*m_br+k];
+			}
+		}
+	}
+
+    double Time2 = omp_get_wtime();
 	sprintf(st, "Time: %3.3f ", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
 	cout << st;
 
@@ -252,6 +310,7 @@ int main (int argc, char *argv[])
 		cout << endl << "1. Multiplication" << endl;
 		cout << "2. Line Multiplication" << endl;
 		cout << "3. Block Multiplication" << endl;
+		cout << "4. Line mult 2" << endl;
 		cout << "Selection?: ";
 		cin >>op;
 		if (op == 0)
@@ -276,6 +335,9 @@ int main (int argc, char *argv[])
 				cout << "Block Size? ";
 				cin >> blockSize;
 				OnMultBlock(lin, col, blockSize);  
+				break;
+			case 4:
+				OnMultLine2(lin,col);
 				break;
 
 		}
